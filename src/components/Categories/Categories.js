f412c20e071a680ec8be5d7009a112f1d0cb6a27
Suad Emre Umar <suadeumar@gmail.com>
2021-10-14 09:22:26 +0200

    Add change

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import Products from '../../data/data.json';
 
 function Categories(props) {
-    // const [products, setProducts] = useState([]);
-
-    // useEffect(() => {
-    //     let items = Products.filter(
-    //         (product) => product.category.tittle === props.match.params.slug
-    //     );
-    //     setProducts(items);
-    // }, [props.match.params.slug]);
-
     return (
         <div>
-            {/* {Products.map((product) => {
-                <li>{product.title}</li>;
-            })} */}
             <ExampleH1>coming soon</ExampleH1>
         </div>
     );
