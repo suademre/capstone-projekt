83d2e1c1083429d33c77f14e84b75c27b2ed0799
Suad Emre Umar <suadeumar@gmail.com>
2021-10-23 13:32:54 +0200

    cardlist created for every pages sorting.

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,71 @@
+import Card from 'components/Card/Card';
+import Filter from 'components/Filter/Filter';
+import Footer from 'components/Footer/Footer';
+import React, {useState} from 'react';
+import styled from 'styled-components/macro';
+
+function CardList({
+    favouriteItems,
+    handleFavoriteButtonClick,
+    basketItems,
+    handleBasketButtonClick,
+    products,
+}) {
+    // const [products, setProducts] = useState([]);
+    const [filterShow, setFilterShow] = useState(false);
+    const [sortByPriceStatus, setSortByPriceStatus] = useState('unsorted');
+
+    // "unsorted" | "ascending" | "descending"
+    const toggleFilterShow = () => {
+        setFilterShow(!filterShow);
+    };
+
+    const filterPrice = (status) => setSortByPriceStatus(status);
+
+    // sort products by price:
+    let shownProducts;
+    if (sortByPriceStatus === 'increase') {
+        shownProducts = [...products].sort((a, b) => a.price - b.price);
+    } else if (sortByPriceStatus === 'decrease') {
+        shownProducts = [...products].sort((a, b) => b.price - a.price);
+    } else {
+        shownProducts = products;
+    }
+
+    return (
+        <>
+            <CardSection>
+                {shownProducts.map((product, id) => (
+                    <Card
+                        product={product}
+                        key={id}
+                        favouriteItems={favouriteItems}
+                        handleFavoriteButtonClick={handleFavoriteButtonClick}
+                        basketItems={basketItems}
+                        handleBasketButtonClick={handleBasketButtonClick}
+                    />
+                ))}
+            </CardSection>
+            <Footer
+                toggleFilterShow={toggleFilterShow}
+                count={shownProducts.length}
+            />
+            <Filter
+                filterShow={filterShow}
+                toggleFilterShow={toggleFilterShow}
+                filterPrice={filterPrice}
+            />
+        </>
+    );
+}
+
+const CardSection = styled.section`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 7rem;
+    margin-bottom: 4rem;
+`;
+
+export default CardList;
