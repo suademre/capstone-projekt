3fce805215feb521a5c6d32c255eb36b89e13941
Suad Emre Umar <suadeumar@gmail.com>
2021-10-25 14:15:17 +0200

    Created ProductDetail Page

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+function ProductDetail({allProducts, categories, ...props}) {
+    const item = props.match.params.item;
+
+    const productDetail = allProducts.find((product) => product.slug === item);
+
+    return (
+        <CardDiv>
+            <CardImg src={productDetail.imageFrond} />
+            <span>
+                {productDetail.title} : Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Fusce in urna purus. Quisque eu sem ac dui
+                pellentesque vulputate id id lacus. Ut eget ipsum consequat,
+                ornare ex nec, rhoncus quam.
+            </span>
+        </CardDiv>
+    );
+}
+
+const CardDiv = styled.div`
+    width: 96%;
+    background-color: #f2f3ee;
+    display: block;
+    color: #000;
+    margin: 10rem auto;
+    box-shadow: 0px 0px 10px #dedede;
+    padding: 2.2rem 1.2rem;
+`;
+
+const CardImg = styled.img`
+    width: 100%;
+`;
+
+export default ProductDetail;
