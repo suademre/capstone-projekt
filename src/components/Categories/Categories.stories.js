1c3bdc60593797f6a539cf46ab5abc57c1f1806a
Suad Emre Umar <suadeumar@gmail.com>
2021-10-27 11:13:47 +0200

    Changed Categories storybook

--- src/components/Categories/Categories.stories.js
+++ src/components/Categories/Categories.stories.js
@@ -1,5 +1,5 @@
 import Categories from './Categories';
-import placeholder from '../../images/placeholder.png';
+/* import placeholder from '../../images/placeholder.png'; */
 
 export default {
     title: 'Component/Categories',
@@ -10,18 +10,45 @@ const Template = (args) => <Categories {...args} />;
 
 export const CategoryItems = Template.bind({});
 CategoryItems.args = {
-    id: '1',
+    allProducts: [
+        {
+            id: 'et0',
+            title: 'Flying Low',
+            price: 105,
+            imageFrond:
+                'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+            slug: 'flying-low',
+            category: {
+                title: 't-shirt',
+            },
+        },
+    ],
     product: {
-        imageFrond: placeholder,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
         title: 'Flying Low',
         price: 105,
+        category: {
+            title: 't-shirt',
+        },
     },
+    title: ['Flying Low', 'basic-shirt'],
     match: {
         params: {
             slug: 't-shirt',
         },
     },
-    favourites: [
+    favouriteItems: [
+        {
+            id: 'et0',
+            title: 'Flying Low',
+            price: 105,
+            imageFrond:
+                'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+            slug: 'flying-low',
+        },
+    ],
+    basketItems: [
         {
             id: 'et0',
             title: 'Flying Low',
@@ -32,4 +59,5 @@ CategoryItems.args = {
         },
     ],
     handleFavoriteButtonClick: () => console.log('access'),
+    handleBasketButtonClick: () => console.log('access'),
 };
