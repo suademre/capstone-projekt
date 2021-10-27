d438dcf43d71c17b859be12712b342d81a984147
Suad Emre Umar <suadeumar@gmail.com>
2021-10-27 11:14:04 +0200

    Changed Favourite Page storybook

--- src/components/Favourite/Favourite.stories.js
+++ src/components/Favourite/Favourite.stories.js
@@ -1,5 +1,4 @@
 import Favourite from './Favourite';
-import placeholder from '../../images/placeholder.png';
 
 export default {
     title: 'Component/Favourite',
@@ -10,18 +9,36 @@ const Template = (args) => <Favourite {...args} />;
 
 export const FavouriteItems = Template.bind({});
 FavouriteItems.args = {
-    id: '1',
-    product: {
-        imageFrond: placeholder,
-        title: 'Flying Low',
-        price: 105,
-    },
+    products: [
+        {
+            id: 'et0',
+            title: 'Flying Low',
+            price: 105,
+            imageFrond:
+                'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+            slug: 'flying-low',
+            category: {
+                title: 't-shirt',
+            },
+        },
+    ],
+
     match: {
         params: {
             slug: 't-shirt',
         },
     },
-    favourites: [
+    favouriteItems: [
+        {
+            id: 'et0',
+            title: 'Flying Low',
+            price: 105,
+            imageFrond:
+                'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+            slug: 'flying-low',
+        },
+    ],
+    basketItems: [
         {
             id: 'et0',
             title: 'Flying Low',
@@ -32,4 +49,5 @@ FavouriteItems.args = {
         },
     ],
     handleFavoriteButtonClick: () => console.log('access'),
+    handleBasketButtonClick: () => console.log('access'),
 };
