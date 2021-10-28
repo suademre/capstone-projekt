361eba0423bcf3d9222d72c6b7ae2b5a5fbca4d9
Suad Emre Umar <suadeumar@gmail.com>
2021-10-28 16:50:07 +0200

    Test for ShoppingCard

---
+++ src/components/ShoppingCard/ShoppingCard.spec.js
@@ -0,0 +1,62 @@
+import {render, screen} from '@testing-library/react';
+import ShoppingCard from './ShoppingCard';
+import {MemoryRouter as Router} from 'react-router-dom';
+
+const favouriteItems = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+    },
+];
+const basketItems = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+    },
+];
+const product = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+        category: {
+            title: 'flying-low',
+        },
+    },
+];
+
+describe('Shopping Card', () => {
+    it('render Shopping Card', () => {
+        const handleBasketButtonClick = jest.fn();
+        const handleFavoriteButtonClick = jest.fn();
+        render(
+            <Router>
+                <ShoppingCard
+                    favouriteItems={favouriteItems}
+                    basketItems={basketItems}
+                    products={product}
+                    handleFavoriteButtonClick={handleFavoriteButtonClick}
+                    handleBasketButtonClick={handleBasketButtonClick}
+                    match={{
+                        params: {
+                            slug: 't-shirt',
+                        },
+                    }}
+                />
+            </Router>
+        );
+        const images = screen.getAllByRole('img');
+        expect(images).toHaveLength(1);
+    });
+});
