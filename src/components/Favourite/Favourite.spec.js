d86b1dfa1a69f3ba76ee2d08ba0b374f84c23214
Suad Emre Umar <suadeumar@gmail.com>
2021-10-28 16:48:06 +0200

    Test for Favourite.js

--- src/components/Favourite/Favourite.spec.js
+++ src/components/Favourite/Favourite.spec.js
@@ -2,24 +2,52 @@ import {render, screen} from '@testing-library/react';
 import Favourite from './Favourite';
 import {MemoryRouter as Router} from 'react-router-dom';
 
-describe('Favourites', () => {
-    const favourites = [
-        {
-            id: 'et0',
-            title: 'Flying Low',
-            price: 105,
-            imageFrond:
-                'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
-            slug: 'flying-low',
+const favouriteItems = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+    },
+];
+const basketItems = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+    },
+];
+const product = [
+    {
+        id: 'et0',
+        title: 'Flying Low',
+        price: 105,
+        imageFrond:
+            'https://www.kaft.com/static/images/cache/800/tisort_flyinglow_19186_800_800.jpg?cacheID=1629358586000',
+        slug: 'flying-low',
+        category: {
+            title: 'flying-low',
         },
-    ];
+    },
+];
+
+describe('Favourites', () => {
     it('render favourites', () => {
         const handleFavoriteButtonClick = jest.fn();
+        const handleBasketButtonClick = jest.fn();
         render(
             <Router>
                 <Favourite
-                    favourites={favourites}
+                    favouriteItems={favouriteItems}
+                    basketItems={basketItems}
+                    products={product}
                     handleFavoriteButtonClick={handleFavoriteButtonClick}
+                    handleBasketButtonClick={handleBasketButtonClick}
                     match={{
                         params: {
                             slug: 't-shirt',
