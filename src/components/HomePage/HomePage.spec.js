f44b48c5685a1ec042f482f6394ad0e8ff1411c6
Suad Emre Umar <suadeumar@gmail.com>
2021-10-29 15:47:07 +0200

    Add attribute testing

--- src/components/HomePage/HomePage.spec.js
+++ src/components/HomePage/HomePage.spec.js
@@ -2,9 +2,15 @@ import {render, screen} from '@testing-library/react';
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
-    it('render hompage', () => {
+    it('renders hompage', () => {
         render(<HomePage />);
         const image = screen.getByRole('img');
         expect(image).toBeInTheDocument();
+    });
+    it('has an alt attribute', () => {
+        render(<HomePage />);
+
+        const alt = screen.getByAltText('HomeImg');
+        expect(alt).toBeInTheDocument();
     });
 });
