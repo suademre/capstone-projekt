d2d5d721b7ccc51ac9c284e8b1ae4dab3a5eab7c
Suad Emre Umar <suadeumar@gmail.com>
2021-10-29 15:46:31 +0200

    Add function testing

--- src/components/Filter/Filter.spec.js
+++ src/components/Filter/Filter.spec.js
@@ -1,8 +1,9 @@
 import {render, screen} from '@testing-library/react';
 import Filter from './Filter';
+import userEvent from '@testing-library/user-event';
 
 describe('Filter', () => {
-    it('render filter', () => {
+    it('renders filter', () => {
         const filterShow = jest.fn();
         const onToggleFilterShow = jest.fn();
         const filterPrice = jest.fn();
@@ -15,5 +16,18 @@ describe('Filter', () => {
         );
         const buttons = screen.getByRole('button');
         expect(buttons).toBeInTheDocument();
+    });
+    it('calls a function when is clicked and filtershow state is false', () => {
+        const mockOnToggleFilterShow = jest.fn();
+        render(
+            <Filter
+                onToggleFilterShow={mockOnToggleFilterShow}
+                filter={false}
+            />
+        );
+
+        const button = screen.getByRole('button');
+        userEvent.click(button);
+        expect(mockOnToggleFilterShow).toHaveBeenCalled();
     });
 });
