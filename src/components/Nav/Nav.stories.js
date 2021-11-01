c000963b3315cf737ce8eabead30e58048dcf0f5
Suad Emre Umar <suadeumar@gmail.com>
2021-11-01 15:53:42 +0100

    Changed Nav.stories.js

--- src/components/Nav/Nav.stories.js
+++ src/components/Nav/Nav.stories.js
@@ -1,7 +1,20 @@
 import Nav from '.';
 
-export default {title: 'Nav', component: Nav};
+export default {title: 'Component/Nav', component: Nav};
 
 const Template = (args) => <Nav {...args} />;
 
 export const menuOpen = Template.bind({});
+menuOpen.args = {
+    categories: [
+        {
+            id: '0',
+            slug: 't-shirt',
+            icon: '',
+            title: 'T-shirt',
+            description:
+                'Bir avuç insan kalmıştık varlığımızı kutlayacak ve bildiğimiz tüm dilleri unutmuştuk.',
+            size: ['S', 'M', 'L', 'XL', '2XL'],
+        },
+    ],
+};
